52438107285dfaee7f4e48aefa8f11b1b7789be4
ehsanwaheed <ehsanw97@gmail.com>
2020-01-30 18:33:39 +0000

    Tab name changed Deleted not needed links and added github url.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Ehsan's Portfolio</title>
+        <title>Ehsan Waheed</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -83,16 +83,8 @@
                 <div class="title m-b-md">
                     Ehsan's Portfolio
                 </div>
-
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/EhsanWaheed">GitHub</a>
                 </div>
             </div>
         </div>
